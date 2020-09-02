941bd15f68d0f98d0fcee58f968789316d24f6bc
Dmitry Makarevich <d.a.makar@list.ru>
2020-09-02 17:26:36 +0300

    add parsing json file

--- ToDo/ViewController.swift
+++ ToDo/ViewController.swift
@@ -8,13 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    //MARK: - Variables
+    private let fileName = "Lists"
+    private var list = [TDCategoryMenu]()
+
+    //MARK: - Life cycle method(s)
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        if let jsonData = self.readLocalFile(forName: fileName) {
+            self.list = self.parse(data: jsonData)
+
+            list.forEach {
+                print("Title: ", $0.title)
+                print("Image: ", $0.image)
+                print("---------------------")
+            }
+        }
+    }
+    
+    //MARK: - Read and parsing json file
+    private func readLocalFile(forName name: String) -> Data? {
+        do {
+            if let bundlePath = Bundle.main.path(forResource: name,
+                                                 ofType: "json"),
+                let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8) {
+
+                return jsonData
+            }
+        } catch {
+            print(error)
+        }
+
+        return nil
     }
 
+    private func parse(data: Data) -> [TDCategoryMenu] {
+        var list = [TDCategoryMenu]()
+
+        do {
+            let decodedData = try JSONDecoder().decode(TDCategoryList.self,
+                                                       from: data)
+            list = decodedData.lists
+        } catch {
+            print("decode error")
+        }
+
+        return list
+    }
 
 }
 
