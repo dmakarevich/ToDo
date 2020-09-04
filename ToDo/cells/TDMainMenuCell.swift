0d45a0c40afd501163e1c5b4daeb36d4f2689912
Dmitry Makarevich <d.a.makar@list.ru>
2020-09-04 19:08:15 +0300

    create main menu screen

---
+++ ToDo/cells/TDMainMenuCell.swift
@@ -0,0 +1,26 @@
+//
+//  TDMainMenuCell.swift
+//  ToDo
+//
+//  Created by Admin on 04.09.2020.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class TDMainMenuCell: UICollectionViewCell {
+    static let reuseIdentifier = "TDMainMenuCell"
+    
+    @IBInspectable
+    private var cornerRadius: CGFloat {
+        get {
+            return self.layer.cornerRadius
+        }
+        set {
+            self.layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBOutlet weak var cellContentView: TDMenuItemView!
+}
---
+++ ToDo/views/TDHeaderMainMenuView.swift
@@ -0,0 +1,16 @@
+//
+//  TDHeaderMainMenuView.swift
+//  ToDo
+//
+//  Created by Admin on 04.09.2020.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+class TDHeaderMainMenuView: UICollectionReusableView {
+    static let reuseIdentifier = "TDHeaderMainMenuView"
+    
+    @IBOutlet weak var headerLabel: UILabel!
+    
+}
